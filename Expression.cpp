79f15d97bef07ff382b7425f195549c58c9e79dd
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2021-04-19 15:53:06 +0300

    öwe wrote the infix to postfix code

--- Expression.cpp
+++ Expression.cpp
@@ -5,53 +5,72 @@
 using namespace std;
 
 string operators = "-+()/*";
+string bigger = "*/";
+string smaller = "-+";
 stack<char> oper;
 
-string postfix(string expr){
-  
-  string postfixExp = "";
-  for(char ch : expr){
+string postfix(string expr)
+{
 
-      if(operators.find(ch) < operators.length()){  // ch bir operator  // değiştir burayı  
+    string postfixExp = "";
+    for (char ch : expr)
+    {
 
-        if(oper.empty()){    // stack boş
-            oper.push(ch);  // ch yi stack a koy
-        } else {  // stack boş değil
+        if (operators.find(ch) < operators.length())
+        { // ch bir operator  // değiştir burayı
 
-            if(ch==')'){
-
-                while(oper.top()!='('){
-
-                    postfixExp = postfixExp + oper.top();
-                    oper.pop();
-                }
-
-                oper.pop();
-
-            } else {
-
-                while( ch!='(' || oper.top()!='(' || !oper.empty() || !(operators.find(ch)-operators.find(oper.top())>1) ){
-
-                    postfixExp = postfixExp + oper.top();
-                    oper.pop();
-
-                }
-
-                oper.push(ch);
+            if (oper.empty())
+            {                  // stack boş
+                oper.push(ch); // ch yi stack a koy
             }
-            
+            else
+            { // stack boş değil
+
+                if (ch == ')')
+                {
+
+                    while (oper.top() != '(')
+                    {
+
+                        postfixExp = postfixExp + oper.top();
+                        oper.pop();
+                    }
+
+                    oper.pop();
+                }
+                else
+                {
+                    // cout << "yeni gelen = " <<operators.find(ch) << "içeride olan = "<< operators.find(oper.top()) << endl;
+
+                    // cout << operators.find(ch)-operators.find(oper.top()) << endl;
+
+                    // cout << !(operators.find(ch)-operators.find(oper.top())>1) << endl;
+
+                    while (!oper.empty() && ch != '(' && oper.top() != '(')
+                    {
+                        if (smaller.find(oper.top()) < 2 && bigger.find(ch) < 2)
+                        {
+                            break;
+                        }
+
+                        postfixExp = postfixExp + oper.top();
+                        oper.pop();
+                    }
+
+                    oper.push(ch);
+                }
+            }
         }
-
-      } else {  //ch bir variable
-          postfixExp = postfixExp + ch;
-      }
-
-
-
+        else
+        { //ch bir variable
+            postfixExp = postfixExp + ch;
+        }
+    }
 
 
-  }
+    return postfixExp + oper.top();
+}
 
-
-
+int evaluate(string str){
+    return 0;
 }
--- test.cpp
+++ test.cpp
@@ -9,11 +9,12 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
     
-    string expr = "K+L-M*(N+W)/U/V*T+Q";
+    string expr = "K+L-M*N+(O*P)*W/U/V*(T+Q)";
+    //string a = "+*-/*-//*/+-";
     string q  = postfix(expr);
     cout << q << endl;  
     
-
+    //KL+MN*-OP*W*U/V/T*+Q+
     
     return 0;
 }
