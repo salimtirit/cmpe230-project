7148f574a402d713b4bff3073a6a1e2394a62bbf
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2021-04-27 09:35:30 +0300

    if statement written as merged with while

--- while.cpp
+++ while.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 
 int whileNamer = 0;
+int ifNamer = 0;
+int nOfLoops = 0;
 
 void assign(string i)
 {
@@ -21,7 +23,7 @@ void assign(string i)
     }
 }
 
-void mainLoop(vector<string> lines, int &lineNumber, int nOfLoops)
+void mainLoop(vector<string> lines, int &lineNumber, int &nOfLoops)
 {
     string i = lines[lineNumber];
     i.erase(remove(i.begin(), i.end(), '\t'), i.end()); // removes all tab characters
@@ -36,25 +38,38 @@ void mainLoop(vector<string> lines, int &lineNumber, int nOfLoops)
     {
         assign(i);
     }
-    else if (i.find("while(") == 0)
-    {
+    else if(i.find("while(") == 0 || i.find("if(") == 0){
         if(nOfLoops){
             cout << "Syntax error in line #" << lineNumber << endl;
             exit(0);
         }
+        nOfLoops++;
         int openPosition = i.find("(");
         int closePosition = i.find(")");
-        if (i.substr(0, openPosition) != "while" || i.substr(closePosition + 1, i.length()) != "{")
+        string ifORwhile[2] = {"if","while"};
+        int choose;
+        int smallNamer;
+        if (i.find("if(") == 0){
+            smallNamer = ifNamer;
+            ifNamer++;
+            choose = 0;
+        }else{
+            smallNamer = whileNamer;
+            whileNamer++;            
+            choose = 1;
+        }
+        if (i.substr(0, openPosition) != ifORwhile[choose] || i.substr(closePosition + 1, i.length()) != "{")
         {
             cout << "Syntax Error in line #" << lineNumber << endl;
             exit(0);
-        }
-        string whileConditionName = "whilecond" + to_string(whileNamer);
-        string whileBodyName = "whilebody" + to_string(whileNamer);
-        string whileEndName = "whileend" + to_string(whileNamer++);
+        }      
 
-        cout << "br label %" + whileConditionName << endl;
-        cout << whileConditionName << ":" << endl;
+        string conditionName = ifORwhile[choose]+"cond" + to_string(smallNamer);
+        string bodyName = ifORwhile[choose]+"body" + to_string(smallNamer);
+        string endName = ifORwhile[choose]+"end" + to_string(smallNamer);  
+
+        cout << "br label %" + conditionName << endl;
+        cout << conditionName << ":" << endl;
         string condition = i.substr(openPosition + 1, closePosition - openPosition - 1);
 
         isValidVariable(condition);
@@ -67,33 +82,37 @@ void mainLoop(vector<string> lines, int &lineNumber, int nOfLoops)
         string s2 = "%" + namerCondition2 + " = icmp ne i32 %" + namerCondition1 + ", 0";
         cout << s2 << endl;
 
-        string s3 = "br i1 %" + namerCondition2 + ", label %" + whileBodyName + ", label %" + whileEndName;
+        string s3 = "br i1 %" + namerCondition2 + ", label %" + bodyName + ", label %" + endName;
         cout << s3 << endl;
 
-        cout << whileBodyName << ":" << endl;
+        cout << bodyName << ":" << endl;
 
         while (!(i.find("}") < i.size()))
         {
             lineNumber++;
             if(lineNumber < lines.size()){
-                mainLoop(lines, lineNumber, nOfLoops++);
+                mainLoop(lines, lineNumber, nOfLoops);
             }else {
                 cout << "No curly bracets in line #"<< lines.size() << endl; // last line number
                 exit(0);
             }
             i = lines[lineNumber];
         }
-        cout << "br label %" + whileConditionName << endl;
-        cout << whileEndName << ":" << endl;
-    }
-    else if (i.find("if("))
-    {
-        // dont forget to increase the number of loops
+
+        nOfLoops--;
+
+        if(choose){
+            cout << "br label %" + conditionName << endl;
+        }else{
+            cout << "br label %" + endName << endl;
+        }
+        cout << endName << ":" << endl;
     }
     else if (i.find("print("))
     {
         //there may be a choose function inside
         //print the given variables
+        
     }
     else if (i == "")
     {
@@ -111,7 +130,7 @@ int main(int argc, char const *argv[])
     cout << "define i32 @main() {" << endl; // main starts
     vector<string> tokens;
 
-    string inputFile = "input1.txt";  //argv[1];
+    string inputFile = "input2.txt";  //argv[1];
     string outputFile = "output.txt"; //argv[2];
 
     ifstream infile;
@@ -131,7 +150,7 @@ int main(int argc, char const *argv[])
     //main for loop
     for (; lineNumber < lines.size(); lineNumber++)
     {
-        mainLoop(lines, lineNumber, 0);
+        mainLoop(lines, lineNumber, nOfLoops);
     }
 
     cout << "ret i32 0" << endl; //return 0
