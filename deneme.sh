aa70f1793c5b42de1eef454ab9e3dd2ade7c8224
Cmpe250 Student <salimtirit58@gmail.com>
2021-05-03 09:53:47 +0300

    I am just playing with theese

--- deneme.sh
+++ deneme.sh
@@ -1,8 +1,8 @@
 make
 for currentCount in {0..18}
 do
-  ./mylang2ir ./inputs/testcase$currentCount.my
+   ./mylang2ir ./inputs/testcase$currentCount.my
    lli inputs/testcase$currentCount.ll > inputs/testcase$currentCount.txt
-    diff -s outputs/testcase$currentCount.txt inputs/testcase$currentCount.txt
+   diff -s outputs/testcase$currentCount.txt inputs/testcase$currentCount.txt
     #
 done
